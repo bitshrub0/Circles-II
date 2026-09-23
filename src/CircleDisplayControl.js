2814dcc517e18ba1b75633187e65d1f844a6acfe
Bitshrub <bitshrub0@gmail.com>
2026-09-23 17:23:28 +0100

    This i

--- src/CircleDisplayControl.js
+++ src/CircleDisplayControl.js
@@ -4,7 +4,7 @@ let progressBars = [document.getElementById("bar0")]
 let progressValues = [D(0)];
 
 function progress(i, x){
-    let hexAdds = [126+(i*(20+(i*4))), (i+247), 180-(i*(15+(i*4)))]
+    let hexAdds = [80+(i*(25+(i*5))), (i+247), 230-(i*(15+(i*5)))]
     progressValues[i]=x;
     valueContainer.textContent = `${format(progressValues[0])}`
     progressBars[i].style.background = `conic-gradient(
