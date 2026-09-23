1a705e961b60664fd26d8a110c663157c3ccbf98
Bitshrub <bitshrub0@gmail.com>
2026-09-23 16:44:32 +0100

    Removed the % (heresy I know but it must be done)

--- src/CircleDisplayControl.js
+++ src/CircleDisplayControl.js
@@ -6,7 +6,7 @@ let progressValues = [D(0)];
 function progress(i, x){
     let hexAdds = [126+(i*(15+(i*4))), i*(15+(i*4)), 180-(i*(10+(i*4)))]
     progressValues[i]=x;
-    valueContainer.textContent = `${format(progressValues[0])}%`
+    valueContainer.textContent = `${format(progressValues[0])}`
     progressBars[i].style.background = `conic-gradient(
       rgb(${hexAdds[0]}, ${hexAdds[1]}, ${hexAdds[2]}) ${progressValues[i].times(3.6)}deg,
       #000 ${progressValues[i].times(3.6)}deg
