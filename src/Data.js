72f7d761394c07dc970176b14ec04472b59b2764
Bitshrub <bitshrub0@gmail.com>
2026-09-24 16:25:02 +0100

    Putting in Jokes

--- src/Data.js
+++ src/Data.js
@@ -1,4 +1,6 @@
 const D = x => new Decimal(x)
+const jokes = ["That's Numberwang", "Might be contaminated", "Hi Flame", "NOTE: Put joke here", "I like this game", "Support Lizard Panini", "Join the Gwa", "EEEEEEEEEEEEEEEEEEEEEEEE", "quack", "360 258 272"] // it's joke time people
+let jokeVal = jokes[getRandomInt(jokes.length)]
 //create all the variables in a data object for saving
 function getDefaultObject() {
     return {
