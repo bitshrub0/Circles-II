7edca6dde6401afcf3ddb8f03e8472217ab435b1
Bitshrub <bitshrub0@gmail.com>
2026-09-25 13:54:04 +0100

    Joke File

---
+++ src/Jokes.js
@@ -0,0 +1,9 @@
+const jokes = ["That's Numberwang", "Might be contaminated", "Hi Flame", "NOTE: Put joke here", "I like this game", "Support Lizard Panini", "Join the Gwa", 
+               "EEEEEEEEEEEEEEEEEEEEEEEE", "quack", "360 258 272", "いいですね。", "Sponsored by KIRIN Milk Tea", "Now with 170% more jokes", "Thanks, Seizure Man", 
+               "https://xkcd.com/505/", "*CRASH*", "Damnation", "Why????", "Back in my day pigeons wore TOP HATS and WAISTCOATS", "Watch this space", "Don't do it!", 
+               "Mmmmm, delicious", "GO OUTSIDE RIGHT NOW AND TOUCH SOME GRASS", "26925134898", "Press Ctrl+Alt+F4 for free Circle 8", "#freefrench", "~~~Hover~~~" ] // it's joke time people
+//get random integer
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+let jokeVal = jokes[getRandomInt(jokes.length)] // jokes!
